f44575a4a07f8ba8913fa65784b1006399f03e14
Zsolt Mihalyi <zsolt.mihalyi@tappointment.com>
2024-01-16 15:34:19 +0100

    chore: add basic rendering

--- app/components/RenderFormElement.tsx
+++ app/components/RenderFormElement.tsx
@@ -1,13 +1,25 @@
-enum FormElement {
+import { FormElement } from "../page";
+
+export enum FormElements {
 	Input = "input",
-	Dropdown = "dropdown",
+	Select = "select",
 }
 
-const RenderFormElement = ({ type }: { type: FormElement }) => {
-	if (type === FormElement.Input) {
-		return <input />;
-	} else if (type === FormElement.Dropdown) {
-		<div>Dropdown</div>;
+export const RenderFormElement = ({
+	element,
+	setState,
+}: {
+	element: FormElement;
+	setState: () => void;
+}) => {
+	console.log("element: ", element);
+	const { type } = element;
+
+	if (type === FormElements.Input) {
+		console.log("element.value: ", element.value);
+		return <input value={element.value} onChange={setState} />;
+	} else if (type === FormElements.Select) {
+		return <div>Select</div>;
 	}
 
 	return <div>Invalid element</div>;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,114 @@
-import Image from "next/image";
-import { Button } from "flowbite-react";
+"use client";
+
+import { Label, Select } from "flowbite-react";
+import {
+	FormElements,
+	RenderFormElement,
+} from "./components/RenderFormElement";
+import { useState } from "react";
+
+const state: State = {
+	forms: [
+		{
+			formId: "1",
+			formName: "User registration",
+			formElements: [
+				{
+					id: "1",
+					label: "Name",
+					type: FormElements.Input,
+					value: "",
+				},
+				{
+					id: "2",
+					label: "Gender",
+					type: FormElements.Select,
+					value: "",
+					values: ["Male", "Female"],
+				},
+				{
+					id: "3",
+					label: "City",
+					type: FormElements.Input,
+					value: "",
+				},
+			],
+		},
+		{
+			formId: "2",
+			formName: "Finance data",
+			formElements: [
+				{
+					id: "1",
+					label: "Revenue 2023",
+					type: FormElements.Input,
+					value: "",
+				},
+				{
+					id: "3",
+					label: "Tax",
+					type: FormElements.Input,
+					value: "1111",
+				},
+			],
+		},
+	],
+};
+
+const renderForm = (formElements: FormElement[]) =>
+	formElements.map((e, i) => (
+		<div key={`element-${i}`} className="my-3">
+			<RenderFormElement element={e} setState={() => {}} />
+		</div>
+	));
 
 export default function Home() {
+	const [selectedTemplate, setSelectedTemplate] = useState<string>("");
+	const selectedForm = state.forms.find((f) => f.formId === selectedTemplate);
+	const selectedFormElements = selectedForm?.formElements;
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Button>Click me</Button>{" "}
-		</main>
+		<div className="flex align-middle flex-col">
+			<div className="max-w-md mb-1">
+				<div className="mb-2 block">
+					<Label htmlFor="templates" value="Select template" />
+				</div>
+				<Select
+					id="templates"
+					required
+					onChange={(e) => {
+						setSelectedTemplate(e.target.value);
+					}}
+					value={selectedTemplate}
+				>
+					{state.forms.map(({ formName, formId }, index) => {
+						return (
+							<option key={formName + index} value={formId}>
+								{formName}
+							</option>
+						);
+					})}
+				</Select>
+			</div>
+			{selectedFormElements && renderForm(selectedFormElements)}
+		</div>
 	);
 }
+
+export type FormElement = {
+	id: string;
+	label: string;
+	type: FormElements;
+	value: string;
+	values?: string[];
+};
+
+type Form = {
+	formId: string;
+	formName: string;
+	formElements: FormElement[];
+};
+
+type State = {
+	forms: Form[];
+};
