954ebb96e642f9b7ef961d7a475dab33e39de632
Zsolt Mihalyi <zsolt.mihalyi@tappointment.com>
2024-01-17 15:32:22 +0100

    chore: adjust code

--- app/page.tsx
+++ app/page.tsx
@@ -86,37 +86,45 @@ const state: State = {
 	],
 };
 
-const renderForm = (
-	formState: Form,
-	setGlobalState: Dispatch<SetStateAction<State>>
-) =>
-	formState.formElements.map((e, i) => {
-		const setElementState = (value: any) => {
-			setGlobalState((prevValue) => {
-				const updatedForms = prevValue.forms.map((f) => {
-					if (f.formId !== formState.formId) {
-						return f;
-					}
+const RenderForm = ({
+	formState,
+	setGlobalState,
+}: {
+	formState: Form;
+	setGlobalState: Dispatch<SetStateAction<State>>;
+}) => {
+	return (
+		<>
+			{formState.formElements.map((e, i) => {
+				const setElementState = (value: any) => {
+					setGlobalState((prevValue) => {
+						const updatedForms = prevValue.forms.map((f) => {
+							if (f.formId !== formState.formId) {
+								return f;
+							}
 
-					const updatedFormElements = f.formElements.map((fe) => {
-						if (e.id !== fe.id) {
-							return fe;
-						}
-						const updatedElement = { ...fe, value };
-						return updatedElement;
+							const updatedFormElements = f.formElements.map((fe) => {
+								if (e.id !== fe.id) {
+									return fe;
+								}
+								const updatedElement = { ...fe, value };
+								return updatedElement;
+							});
+							return { ...f, formElements: updatedFormElements };
+						});
+
+						return { ...prevValue, forms: updatedForms };
 					});
-					return { ...f, formElements: updatedFormElements };
-				});
-
-				return { ...prevValue, forms: updatedForms };
-			});
-		};
-		return (
-			<div key={`element-${i}`} className="my-3">
-				<RenderFormElement element={e} setElementState={setElementState} />
-			</div>
-		);
-	});
+				};
+				return (
+					<div key={`element-${i}`} className="my-3">
+						<RenderFormElement element={e} setElementState={setElementState} />
+					</div>
+				);
+			})}
+		</>
+	);
+};
 
 export default function Home() {
 	const [selectedTemplate, setSelectedTemplate] =
@@ -154,9 +162,12 @@ export default function Home() {
 					})}
 				</Select>
 			</div>
-			{selectedFormStateElements &&
-				selectedFormStateId &&
-				renderForm(selectedFormState, setGlobalState)}
+			{selectedFormState && (
+				<RenderForm
+					formState={selectedFormState}
+					setGlobalState={setGlobalState}
+				/>
+			)}
 		</div>
 	);
 }
