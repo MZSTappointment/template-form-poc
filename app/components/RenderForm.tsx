d1f93eeb9c104763729d851df613d46e1d85bd48
Zsolt Mihalyi <zsolt.mihalyi@tappointment.com>
2024-02-21 11:05:39 +0100

    dnd wip

--- app/components/RenderForm.tsx
+++ app/components/RenderForm.tsx
@@ -1,6 +1,22 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Form, State } from "../page";
 import { RenderFormElement } from "./RenderFormElement";
+import { SortableItem } from "../components/SortableItem";
+import {
+	DndContext,
+	closestCenter,
+	KeyboardSensor,
+	PointerSensor,
+	useSensor,
+	useSensors,
+	DragEndEvent,
+} from "@dnd-kit/core";
+import {
+	arrayMove,
+	SortableContext,
+	sortableKeyboardCoordinates,
+	verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 export const RenderForm = ({
 	formState,
@@ -9,35 +25,81 @@ export const RenderForm = ({
 	formState: Form;
 	setGlobalState: Dispatch<SetStateAction<State>>;
 }) => {
+	const sensors = useSensors(
+		useSensor(PointerSensor),
+		useSensor(KeyboardSensor, {
+			coordinateGetter: sortableKeyboardCoordinates,
+		})
+	);
+
+	function handleDragEnd(event: DragEndEvent) {
+		const { active, over } = event;
+
+		if (active.id !== over.id) {
+			setGlobalState((prevValue) => {
+				const updatedForms = prevValue.forms.map((f) => {
+					if (f.formId !== formState.formId) {
+						return f;
+					}
+
+					const oldIndex = f.formElements.findIndex((e) => e.id === active.id);
+					const newIndex = f.formElements.findIndex((e) => e.id === over.id);
+					const updatedFormElements = arrayMove(
+						f.formElements,
+						oldIndex,
+						newIndex
+					);
+					return { ...f, formElements: updatedFormElements };
+				});
+
+				return { ...prevValue, forms: updatedForms };
+			});
+		}
+	}
+
 	return (
-		<>
-			{formState.formElements.map((e, i) => {
-				const setElementState = (value: any) => {
-					setGlobalState((prevValue) => {
-						const updatedForms = prevValue.forms.map((f) => {
-							if (f.formId !== formState.formId) {
-								return f;
-							}
-
-							const updatedFormElements = f.formElements.map((fe) => {
-								if (e.id !== fe.id) {
-									return fe;
+		<DndContext
+			sensors={sensors}
+			collisionDetection={closestCenter}
+			onDragEnd={handleDragEnd}
+		>
+			<SortableContext
+				items={formState.formElements.map((e) => e.id)}
+				strategy={verticalListSortingStrategy}
+			>
+				{formState.formElements.map((e, i) => {
+					const setElementState = (value: any) => {
+						setGlobalState((prevValue) => {
+							const updatedForms = prevValue.forms.map((f) => {
+								if (f.formId !== formState.formId) {
+									return f;
 								}
-								const updatedElement = { ...fe, value };
-								return updatedElement;
-							});
-							return { ...f, formElements: updatedFormElements };
-						});
 
-						return { ...prevValue, forms: updatedForms };
-					});
-				};
-				return (
-					<div key={`element-${i}`} className="my-3">
-						<RenderFormElement element={e} setElementState={setElementState} />
-					</div>
-				);
-			})}
-		</>
+								const updatedFormElements = f.formElements.map((fe) => {
+									if (e.id !== fe.id) {
+										return fe;
+									}
+									const updatedElement = { ...fe, value };
+									return updatedElement;
+								});
+								return { ...f, formElements: updatedFormElements };
+							});
+
+							return { ...prevValue, forms: updatedForms };
+						});
+					};
+					return (
+						<SortableItem key={e.id} id={e.id}>
+							<div key={`element-${i}`} className="my-3">
+								<RenderFormElement
+									element={e}
+									setElementState={setElementState}
+								/>
+							</div>
+						</SortableItem>
+					);
+				})}
+			</SortableContext>
+		</DndContext>
 	);
 };
